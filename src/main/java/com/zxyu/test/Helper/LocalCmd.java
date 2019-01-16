da46efbeb988bedbf0dd8f906f1abef714717231
njuzxy <151250199@snail.nju.edu.cn>
2019-01-16 20:30:40 +0800

    java代码质量

--- src/main/java/com/zxyu/test/Helper/LocalCmd.java
+++ src/main/java/com/zxyu/test/Helper/LocalCmd.java
@@ -1,16 +1,14 @@
 package com.zxyu.test.Helper;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class LocalCmd {
     public static void main(String args[])throws Exception{
-        String cmdStr="pylint --rcfile=C:\\Users\\zxyu\\pylint.conf i:\\testpy.py";
+        String cmdStr=args[0];/*"pylint --rcfile=C:\\Users\\zxyu\\pylint.conf i:\\testpy.py";*/
         //String cmdStr="java -version";
         Process process= Runtime.getRuntime().exec("cmd /c "+cmdStr);
-        System.out.println("1");
         InputStream inputStream = process.getInputStream();
         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
         BufferedReader br = new BufferedReader(inputStreamReader);
---
+++ src/main/java/com/zxyu/test/Helper/PmdHelper.java
@@ -0,0 +1,104 @@
+package com.zxyu.test.Helper;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PmdHelper {
+    private static  final int ruleNum=10;
+    private static DocumentBuilderFactory factory=null;
+    private static DocumentBuilder db=null;
+    private static Document document=null;
+
+    static{
+        try{
+            factory=DocumentBuilderFactory.newInstance();
+            db=factory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String command;
+    private String srcPath;
+    private String outputPath;
+    private String rulePath;
+    public PmdHelper(String srcPath,String outputPath,String rulePath) {
+        this.srcPath=srcPath;
+        this.outputPath=outputPath;
+        this.rulePath=rulePath;
+        this.command="pmd -d "+srcPath+" -f xml "+"-R "+rulePath+" -reportfile "+outputPath;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public String getSrcPath() {
+        return srcPath;
+    }
+
+    public void setSrcPath(String srcPath) {
+        this.srcPath = srcPath;
+    }
+
+    public String getOutputPath() {
+        return outputPath;
+    }
+
+    public void setOutputPath(String outputPath) {
+        this.outputPath = outputPath;
+    }
+
+    public void execPmd(){
+        String[]input=new String[1];
+        input[0]=command;
+        System.out.println("command:"+command);
+        try {
+            LocalCmd.main(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getScore(){
+        Set<String> ruleSet=new HashSet<String>() ;
+        try {
+            document=db.parse(outputPath);
+            NodeList violationList=document.getElementsByTagName("violation");
+            Node node;
+            String rule="";
+            for(int i=0;i<violationList.getLength();i++){
+                node=violationList.item(i);
+                NamedNodeMap map=node.getAttributes();
+                rule=map.getNamedItem("rule").getTextContent();
+                ruleSet.add(rule);
+            }
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ruleNum-ruleSet.size();
+    }
+
+    public static void main(String[]args){
+        PmdHelper pmdHelper=new PmdHelper("i:\\MyLauncher.java","i:\\output.xml","c:\\Users\\zxyu\\Desktop\\rule.xml");
+        pmdHelper.execPmd();
+        System.out.println(pmdHelper.getScore());
+    }
+
+}
